92b690f9f0d6901ce7255b6c809827eb737ce411
Fabio Mancinelli <fabio@mancinelli.me>
2012-11-05 00:29:58 +0100

    Add problem 2 solution (Haskell)

---
+++ haskell/project-euler/Problem2.hs
@@ -0,0 +1,18 @@
+module Main where
+
+-- Recursive definition of an infinite list containing the fibonacci sequence. Here how it works:
+-- Step0) 0
+-- Step1) 0 : scanl (+) 1 [0] = 0 : [1, 1] = [0, 1, 1]
+-- Step2) 0 : scanl (+) 1 [0, 1, 1] = 0 : [1, 1, 2, 3] = [0, 1, 1, 2, 3]
+-- Step3) 0 : scanl (+) 1 [0, 1, 1, 2, 3] = 0 : [1, 1, 2, 3, 5, 8] = [0, 1, 1, 2, 3, 5, 8]
+-- ...
+fibonacci  = 0 : scanl (+) 1 fibonacci
+
+-- We use $ in order to remove some parenthesis. $ is equivalent to function application
+-- but it's right associative. Without $, we should have written the following expression in this way:
+-- problem2 = foldr (+) 0 (filter (\x -> x `rem` 2 == 0) (takeWhile (< 4000000) fibonacci))
+problem2  = foldr (+) 0 $ filter (\x -> x `rem` 2 == 0) $ takeWhile (< 4000000) fibonacci
+
+main = do
+     print problem2
+          
