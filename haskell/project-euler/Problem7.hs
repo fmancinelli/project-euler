addff2188ca7f38b2c3a8b69da64cba31ed44a30
Fabio Mancinelli <fabio@mancinelli.me>
2012-11-13 22:45:42 +0100

    Add problem 7 solution (Haskell)

---
+++ haskell/project-euler/Problem7.hs
@@ -0,0 +1,26 @@
+module Main where
+
+------------------------------------------------------------------------------
+
+-- A function that returns True if its parameter is a prime number
+isPrime::Integer -> Bool
+isPrime x 
+	| x <= 3 = True
+	| x > 3  = not $ any (`divides` x) $ factorsToCheck
+	where		
+		factorsToCheck = [2..ceiling $ sqrt $ fromIntegral x]
+		divides x y = y `rem` x == 0
+
+------------------------------------------------------------------------------
+
+-- The list of prime numbers
+primes = filter isPrime [2..]
+
+------------------------------------------------------------------------------
+
+problem7 = last $ take 10001 primes
+
+------------------------------------------------------------------------------
+
+main = do
+	print problem7
