8bde84b0c1ca6107f582a983fa67aed61d862eb1
Fabio Mancinelli <fabio@mancinelli.me>
2012-11-13 19:57:36 +0100

    Add problem 5 solution (Haskell)

---
+++ haskell/project-euler/Problem5.hs
@@ -0,0 +1,18 @@
+import Data.List
+
+------------------------------------------------------------------------------
+
+-- Returns true if x is divisible by [1..n]
+checkFactors x n
+	| n == 1 = True
+	| (x `rem` n) == 0 = checkFactors x (n - 1)
+	| otherwise = False
+
+------------------------------------------------------------------------------
+
+problem5 = find (flip checkFactors 20) [1..]
+
+------------------------------------------------------------------------------
+
+main = do
+	print problem5
